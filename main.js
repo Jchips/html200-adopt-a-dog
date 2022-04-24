05c512d4b4cfa0460693e40668141f1763c7e05d
jrrose <jrrose@uw.edu>
2022-04-24 16:14:48 -0700

    added user persona and testing.txt

--- main.js
+++ main.js
@@ -14,3 +14,9 @@ $('.button').click(function() {
   $("#checkout-total").text(total)
 })
 
+$('.more-dogs-btn').mouseover(function() {
+  $(this).css('box-shadow', '5px 5px 2px #000')
+})
+.mouseout(function() {
+  $(this).css('box-shadow', '0px 0px 0px #000')
+})
